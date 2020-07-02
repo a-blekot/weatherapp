7735072270cac153035230b6189654323674102d
anadi-krishna <akdas.nrs@gmail.com>
2020-07-02 13:42:11 +0300

    1. Cities are now in separeted resource files. \n2. Cities list updates correctly when you select country.

--- app/src/main/java/com/anadi/weatherinfo/Location.java
+++ app/src/main/java/com/anadi/weatherinfo/Location.java
@@ -3,13 +3,11 @@ package com.anadi.weatherinfo;
 import android.content.Context;
 import android.util.Log;
 
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONException;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -28,69 +26,52 @@ public class Location {
     }
 
     public static void loadLocations() {
-//        String jsonString = convert(context.getResources().openRawResource(R.raw.countries_codes));
-////        Log.d("anadi_location", jsonString);
-//
-//        try {
-////            Log.d("anadi_location", "I`.m here!");
-//            JSONArray array = new JSONArray(jsonString);
-//            JSONObject obj;
-//            for (int i = 0; i < array.length(); i++) {
-//                obj = array.getJSONObject(i);
-//                countries.add(new Country(obj.getString("name"), obj.getString("code")));
-//            }
-//
-////            Log.d("anadi_location", countries.toString());
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//        }
-
-        countries.add(new Country("Ukraine", "UA"));
-        countries.add(new Country("Brasil", "BR"));
-
-        cities.put(countries.get(0).toString(), new ArrayList<String>(Arrays.asList(new String[]{"Kiev", "Harkiv"})));
-        cities.put(countries.get(1).toString(), new ArrayList<String>(Arrays.asList(new String[]{"Rio", "Bucho"})));
-
-
-
-//        jsonString = convert(context.getResources().openRawResource(R.raw.cities));
+        String jsonString = convert(context.getResources().openRawResource(R.raw.countries_codes));
 //        Log.d("anadi_location", jsonString);
 
+        try {
+//            Log.d("anadi_location", "I`.m here!");
+            JSONArray array = new JSONArray(jsonString);
+            JSONObject obj;
+            for (int i = 0; i < array.length(); i++) {
+                obj = array.getJSONObject(i);
+                countries.add(new Country(obj.getString("name"), obj.getString("code")));
+            }
 
-//        try {
-////            Log.d("anadi_location", "I`.m here!");
-//            JSONObject obj = new JSONObject(jsonString);
-//
-////            for (Country country: countries) {
-////                JSONArray array = obj.getJSONArray(country.name);
-////                if (null == array) {
-//////                    countries.remove(c);
-////                    Log.d("anadi_location", "There are no cities for country: " + country.name);
-////                    continue;
-////                }
-////
-////                ArrayList<String> cityArray = new ArrayList<>();
-////                for (int i = 0; i < array.length(); i++)
-////                    cityArray.add(array.getString(i));
-////
-////                cities.put(country.toString(), cityArray);
-////            }
-//
-//            JSONArray array = obj.getJSONArray("Albania");
-//            ArrayList<String> cityArray = new ArrayList<>();
-//            for (int i = 0; i < array.length(); i++)
-//                cityArray.add(array.getString(i));
-//
-//            cities.put("Albania", cityArray);
-//
-//
-//            Log.d("anadi_location", cities.get("Albania").toString());
-////            Log.d("anadi_location", cities.get("Ukraine").toString());
-//
-////            Log.d("anadi_location", countries.toString());
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//        }
+//            Log.d("anadi_location", countries.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            for (Country country: countries) {
+                String resourceName = country.name.toLowerCase().replace(" ","_");
+
+                jsonString = convert(context.getResources().openRawResource(
+                        context.getResources().getIdentifier(
+                                resourceName, "raw", context.getPackageName())));
+
+                JSONObject obj = new JSONObject(jsonString);
+                JSONArray array = obj.getJSONArray(country.name);
+
+                if (null == array) {
+//                    countries.remove(c);
+                    Log.d("anadi_location", "There are no cities for country: " + country.name);
+                    continue;
+                }
+
+                ArrayList<String> cityArray = new ArrayList<>();
+                for (int i = 0; i < array.length(); i++)
+                    cityArray.add(array.getString(i));
+
+                cities.put(country.toString(), cityArray);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     private static String convert(InputStream inputStream) {
@@ -101,7 +82,7 @@ public class Location {
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
             while ((line = bufferedReader.readLine()) != null) {
                 stringBuilder.append(line);
-                Log.d("anadi_json", line);
+//                Log.d("anadi_json", line);
             }
         }
         catch (IOException e) {
@@ -113,7 +94,6 @@ public class Location {
     }
 
     public static ArrayList<String> getCityNames(String country) {
-
 
         Log.d("anadi_cities", "get cities for: " + country);
 
