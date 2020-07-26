d02f8762bcacaef6e853d3b91fc4bb02cb084649
anadi-krishna <akdas.nrs@gmail.com>
2020-07-26 21:02:46 +0300

    Simple animation added in DetailsActivity

--- app/src/main/java/com/anadi/weatherinfo/details/DetailsActivity.java
+++ app/src/main/java/com/anadi/weatherinfo/details/DetailsActivity.java
@@ -1,32 +1,34 @@
 package com.anadi.weatherinfo.details;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.AnimationSet;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.anadi.weatherinfo.R;
 import com.anadi.weatherinfo.repository.IconMap;
 import com.anadi.weatherinfo.repository.data.WeatherInfo;
 
 import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.TimeZone;
 
 import timber.log.Timber;
 
-public class DetailsActivity extends AppCompatActivity implements DetailsContract.View{
+public class DetailsActivity extends AppCompatActivity implements DetailsContract.View {
 
     private DetailsContract.Presenter presenter;
     private int id;
-    
+
     private ImageView weatherIconIv;
     private ImageView windDegreeIv;
     private TextView locationNameTv;
@@ -39,7 +41,7 @@ public class DetailsActivity extends AppCompatActivity implements DetailsContrac
     private TextView sysSunriseTv;
     private TextView sysSunsetTv;
     private TextView coordinatesTv;
-    
+
     private ProgressBar progressBar;
 
     @Override
@@ -94,8 +96,8 @@ public class DetailsActivity extends AppCompatActivity implements DetailsContrac
         windDegreeIv.setRotation(info.wind.deg);
         locationNameTv.setText(getString(R.string.details_location_name, info.name, info.sys.country));
 
-        int minutes = info.timezone/60;
-        int hours = minutes/60;
+        int minutes = info.timezone / 60;
+        int hours = minutes / 60;
         minutes %= 60; // to get right time in format hh:mm.
 
         String sign = (hours > 0) ? "+" : (hours < 0) ? "-" : "";
@@ -118,6 +120,75 @@ public class DetailsActivity extends AppCompatActivity implements DetailsContrac
     }
 
     public void update(View view) {
+
+        ObjectAnimator animation = ObjectAnimator.ofFloat(windDegreeIv,
+                "rotation",
+                windDegreeIv.getRotation()-720f);
+        animation.setDuration(500);
+        animation.start();
+
+        createAnimation();
+
         presenter.update();
+    }
+
+    private void createAnimation() {
+        if (weatherIconIv == null)
+            return;
+
+        switch (weatherIconIv.getVisibility()) {
+            case View.VISIBLE:
+                animationHideImage();
+                break;
+
+//            case View.INVISIBLE:
+//                animationShowImage();
+//                break;
+        }
+    }
+
+    private void animationShowImage() {
+// Get the center for the clipping circle.
+        int cx = weatherIconIv.getWidth() / 2;
+        int cy = weatherIconIv.getHeight() / 2;
+
+// Get the final radius for the clipping circle.
+        float finalRadius = (float) Math.hypot(cx, cy);
+
+// Create the animator for this view (the start radius is zero).
+        Animator anim = ViewAnimationUtils
+                .createCircularReveal(weatherIconIv, cx, cy, 0, finalRadius);
+
+// Make the view visible and start the animation.
+        weatherIconIv.setVisibility(View.VISIBLE);
+        anim.start();
+    }
+
+    private void animationHideImage() {
+        // Previously visible view
+
+        // Get the center for the clipping circle.
+        int cx = weatherIconIv.getWidth() / 2;
+        int cy = weatherIconIv.getHeight() / 2;
+
+        // Get the initial radius for the clipping circle.
+        float initialRadius = (float) Math.hypot(cx, cy);
+
+        // Create the animation (the final radius is zero.
+        Animator anim = ViewAnimationUtils.
+                createCircularReveal(weatherIconIv, cx, cy, initialRadius, 0);
+
+        // Make the view invisible when the animation is done.
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                weatherIconIv.setVisibility(View.INVISIBLE);
+                animationShowImage();
+            }
+        });
+
+        // Start the animation.
+        anim.start();
     }
 }
