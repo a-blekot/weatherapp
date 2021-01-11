91faa1cea60905acfa8ce060af38922d3ffd35c3
Blekot Aleksey <Aleksey.BLEKOT@intrasoft-intl.com>
2021-01-11 20:32:35 +0200

    fake commit

--- app/src/test/java/com/anadi/weatherinfo/ExampleUnitTest.java
+++ app/src/test/java/com/anadi/weatherinfo/ExampleUnitTest.java
@@ -12,11 +12,6 @@ import static org.junit.Assert.assertEquals;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-        assertEquals(6, 3 + 3);
+        assertEquals(4, 2 + 2);
     }
-
-//    @Test
-//    public void addition_isWrong() {
-//        assertEquals(4, 1 + 2);
-//    }
 }
